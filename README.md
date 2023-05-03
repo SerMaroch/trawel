71e86f46efe0979363e0ee7830c59fff2d9e5772
SerMaroch <117520156+SerMaroch@users.noreply.github.com>
2023-05-03 10:04:43 +0500

    Первоначальные файлы для проекта MakeCode

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Открыть эту страницу в [https://sermaroch.github.io/trawel/](https://sermaroch.github.io/trawel/)
+
+## Использовать это расширение
+
+Этот репозиторий может быть добавлен в MakeCode как **расширение**.
+
+* открыть [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* нажмите на **Новый проект**
+* нажмите **Расширения** в меню-шестерёнке
+* найдите **https://github.com/sermaroch/trawel** и импортируйте
+
+## Править этот проект ![Значок статуса сборки](https://github.com/sermaroch/trawel/workflows/MakeCode/badge.svg)
+
+Редактировать этот репозиторий в MakeCode.
+
+* открыть [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* нажмите на **Импорт**, затем **Импорт URL**
+* вставьте **https://github.com/sermaroch/trawel** и нажмите Импорт
+
+## Предварительный просмотр блоков
+
+Это изображение показывает блоки кода из последнего коммита в главной ветке.
+Обновление этого изображения может занять несколько минут.
+
+![Отрисовка в виде блоков](https://github.com/sermaroch/trawel/raw/master/.github/makecode/blocks.png)
+
+#### Метаданные (используются для поиска, рендеринга)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,56 @@
+let левый = 0
+let центальный_левый = 0
+let центальный_правый = 0
+let правый = 0
+StartbitV2.startbit_Init()
+StartbitV2.lineFollow_iic_init(StartbitV2.startbit_iic.port4)
+let черный = 0
+let белый = 1
+let перекрестки = 0
+basic.forever(function () {
+    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White)) {
+        правый = черный
+    } else {
+        правый = белый
+    }
+    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White)) {
+        центальный_правый = черный
+    } else {
+        центальный_правый = белый
+    }
+    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
+        центальный_левый = черный
+    } else {
+        центальный_левый = белый
+    }
+    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
+        левый = черный
+    } else {
+        левый = белый
+    }
+    if (левый == белый && правый == белый) {
+        if (центальный_левый == черный && центальный_правый == черный) {
+            StartbitV2.startbit_setMotorSpeed(100, 100)
+        } else if (центальный_левый == черный && центальный_правый == белый) {
+            StartbitV2.startbit_setMotorSpeed(30, 100)
+        } else if (центальный_левый == белый && центальный_правый == черный) {
+            StartbitV2.startbit_setMotorSpeed(100, 30)
+        } else {
+            StartbitV2.startbit_setMotorSpeed(30, 30)
+        }
+    } else {
+        if (левый == черный && правый == черный) {
+            StartbitV2.startbit_setMotorSpeed(92, 100)
+            basic.pause(200)
+            StartbitV2.startbit_setMotorSpeed(0, 0)
+            перекрестки += 1
+            basic.showNumber(перекрестки)
+        } else if (левый == черный && правый == белый) {
+            StartbitV2.startbit_setMotorSpeed(-100, 100)
+        } else if (левый == белый && правый == черный) {
+            StartbitV2.startbit_setMotorSpeed(100, -100)
+        } else {
+            StartbitV2.startbit_setMotorSpeed(20, 20)
+        }
+    }
+})
