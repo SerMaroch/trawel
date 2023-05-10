a6e59c05356e81f9943307587e024957e59c9965
SerMaroch <117520156+SerMaroch@users.noreply.github.com>
2023-05-10 10:41:02 +0500

    Обновить main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,26 +7,50 @@ StartbitV2.lineFollow_iic_init(StartbitV2.startbit_iic.port4)
 let черный = 0
 let белый = 1
 let перекрестки = 0
+let Инверсия = 0
 basic.forever(function () {
-    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White)) {
-        правый = черный
+    if (Инверсия == 0) {
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White)) {
+            правый = белый
+        } else {
+            правый = черный
+        }
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White)) {
+            центальный_правый = белый
+        } else {
+            центальный_правый = черный
+        }
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
+            центальный_левый = белый
+        } else {
+            центальный_левый = черный
+        }
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
+            левый = белый
+        } else {
+            левый = черный
+        }
     } else {
-        правый = белый
-    }
-    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White)) {
-        центальный_правый = черный
-    } else {
-        центальный_правый = белый
-    }
-    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
-        центальный_левый = черный
-    } else {
-        центальный_левый = белый
-    }
-    if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
-        левый = черный
-    } else {
-        левый = белый
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S1, StartbitV2.startbit_LineColor.White)) {
+            правый = черный
+        } else {
+            правый = белый
+        }
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S2, StartbitV2.startbit_LineColor.White)) {
+            центальный_правый = черный
+        } else {
+            центальный_правый = белый
+        }
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S3, StartbitV2.startbit_LineColor.White)) {
+            центальный_левый = черный
+        } else {
+            центальный_левый = белый
+        }
+        if (StartbitV2.startbit_line_followers(StartbitV2.startbit_LineFollowerSensors.S4, StartbitV2.startbit_LineColor.White)) {
+            левый = черный
+        } else {
+            левый = белый
+        }
     }
     if (левый == белый && правый == белый) {
         if (центальный_левый == черный && центальный_правый == черный) {
@@ -40,15 +64,16 @@ basic.forever(function () {
         }
     } else {
         if (левый == черный && правый == черный) {
-            if (перекрестки == 2) {
+            StartbitV2.startbit_setMotorSpeed(92, 100)
+            basic.pause(100)
+            StartbitV2.startbit_setMotorSpeed(0, 0)
+            перекрестки += 1
+            basic.showNumber(перекрестки)
+            if (перекрестки == 1) {
                 StartbitV2.startbit_setMotorSpeed(100, -100)
-                basic.pause(700)
-            } else {
-                StartbitV2.startbit_setMotorSpeed(92, 100)
-                basic.pause(200)
-                StartbitV2.startbit_setMotorSpeed(0, 0)
-                перекрестки += 1
-                basic.showNumber(перекрестки)
+                basic.pause(500)
+            } else if (перекрестки == 2) {
+                Инверсия = 1
             }
         } else if (левый == черный && правый == белый) {
             StartbitV2.startbit_setMotorSpeed(-100, 100)
